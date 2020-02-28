7f56bba2554be240106de0bb44d9d59faa7dabcc
henry Gallo <gallohenry@gmail.com>
2020-02-28 06:25:08 -0500

    added target type

--- alb.tf
+++ alb.tf
@@ -30,6 +30,7 @@ resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.terraformdemo.arn
   port              = var.alb_port
   protocol          = var.alb_protocol
+  target_type       = "instance"
   #ssl_policy        = "ELBSecurityPolicy-2016-08"
   #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
 
