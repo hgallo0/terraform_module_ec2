829daf0e1fd5982ee447176327c6e0c62fa78d0c
henry Gallo <gallohenry@gmail.com>
2020-02-27 20:55:39 -0500

    enabled ec2 health check

--- autoscaling_group.tf
+++ autoscaling_group.tf
@@ -3,7 +3,7 @@ resource "aws_autoscaling_group" "bastion" {
   max_size                  = var.max_size
   min_size                  = var.min_size
   health_check_grace_period = 300
-  #health_check_type         = "EC2"
+  health_check_type         = "EC2"
   desired_capacity     = var.desired_capacity
   force_delete         = true
   launch_configuration = aws_launch_configuration.as_conf.name
